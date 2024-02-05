997f9f1feee24aaf2e1ee7e2d6091e061fb73dfb
Nataliasalvie <natalia.turcanu@mail.ru>
2024-02-05 22:24:46 +0200

    My First Java Project

---
+++ src/my_automation/Test_1.java
@@ -0,0 +1,15 @@
+package my_automation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.curs.md/ro");
+		
+
+	}
+
+}
